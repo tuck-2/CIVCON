d3910d0a5d776ed5410b369c70e81d907e27ac4b
EC2 Default User <ec2-user@ip-172-31-42-120.ap-northeast-1.compute.internal>
2021-06-11 23:58:36 +0000

    [update]userサインアップのストロングパラメータを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :group, :group_kana, :department, :phone_num, :fax_num, :postal_code, :address])
   end
 end
