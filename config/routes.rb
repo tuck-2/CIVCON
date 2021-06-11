53837e547aad478a03e1dbab106128161003b353
EC2 Default User <ec2-user@ip-172-31-42-120.ap-northeast-1.compute.internal>
2021-06-11 17:44:38 +0000

    [add]deviseおよびUser,Companyの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :companies, controllers: {
+    sessions: 'companies/sessions',
+    passwords: 'companies/passwords',
+    registrations: 'companies/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
