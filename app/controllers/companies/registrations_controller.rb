1094035ea6700a056356d26514d7ea71e0721990
EC2 Default User <ec2-user@ip-172-31-42-120.ap-northeast-1.compute.internal>
2021-06-12 01:59:40 +0000

    [add]Companyログイン画面およびリンクの追加

--- app/controllers/companies/registrations_controller.rb
+++ app/controllers/companies/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Companies::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -59,4 +60,11 @@ class Companies::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :postal_code, :address, :phone_num, :fax_num])
+  end
+
 end
