ad56990760fe2da4a77f5aae4f82b7f44437db2e
EC2 Default User <ec2-user@ip-172-31-42-120.ap-northeast-1.compute.internal>
2021-06-16 01:33:46 +0000

    [add]Typeモデルのアソシエーションを記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :company
   has_many :product_images, dependent: :destroy
+  belongs_to :type
   accepts_attachments_for :product_images, attachment: :image
 end
